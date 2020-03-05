7c19c9d2b7ce4de49bc8e2e68c82d5042e64c1eb
abukato <artur.bukato@gmail.com>
2020-03-05 13:42:32 +0300

    feat: task brackets done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let length = str.length;
+  let config = bracketsConfig.map(el => el.join(''));
+
+  for (let i = 0; i < length; i+=2) {
+    for (let j = 0; j < config.length; j++) {
+      str = str.replace(config[j], '');
+    }
+    if (str.length === 0) {
+      return true;
+    }
+  }
+
+  return false;
 }
+
+
